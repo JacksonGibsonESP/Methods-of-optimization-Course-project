4a14eb706da712155d82091213ceb58ace00d392
JacksonGibsonESP <nikich911@gmail.com>
2017-04-25 19:06:23 +0300

    Functions corrected

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import numpy as np
 from numpy.linalg import inv
 
 n = 9
-d = 15
-# l1 = 5
-# l2 = 5
-# l3 = 5
-l1 = 3.20156211871642
-l2 = 16.5151445649137
-l3 = 17.6139149538085
-A1 = -10
+d = 20
+l1 = 5
+l2 = 5
+l3 = 5
+A1 = 0
 A2 = 0
-B1 = 0
-B2 = 10 * sqrt(3)
-C1 = 10
+B1 = 10
+B2 = 17.32050807568877
+C1 = 20
 C2 = 0
 
 a1 = Symbol('a1')
@@ -33,14 +30,11 @@ f1 = (a1 - b1) ** 2 + (a2 - b2) ** 2 + (a3 - b3) ** 2 - d ** 2
 f2 = (a1 - c1) ** 2 + (a2 - c2) ** 2 + (a3 - c3) ** 2 - d ** 2
 f3 = (b1 - c1) ** 2 + (b2 - c2) ** 2 + (b3 - c3) ** 2 - d ** 2
 f4 = (a1 - A1) ** 2 + (a2 - A2) ** 2 + a3 ** 2 - l1 ** 2
-f5 = (b1 - A1) ** 2 + (b2 - A2) ** 2 + b3 ** 2 - l2 ** 2
-f6 = (c1 - A1) ** 2 + (c2 - A2) ** 2 + c3 ** 2 - l3 ** 2
-# f4 = (a1 - A1) ** 2 + (a2 - A2) ** 2 + a3 ** 2
-# f5 = (b1 - A1) ** 2 + (b2 - A2) ** 2 + b3 ** 2
-# f6 = (c1 - A1) ** 2 + (c2 - A2) ** 2 + c3 ** 2
-f7 = (a1 - b1) ** 2 + (a2 - b2) ** 2 - (a1 - c1) ** 2 - (a2 - c2) ** 2
-f8 = (b1 - a1) ** 2 + (b2 - a2) ** 2 - (b1 - c1) ** 2 - (b2 - c2) ** 2
-f9 = (c1 - a1) ** 2 + (c2 - a2) ** 2 - (c1 - b1) ** 2 - (c2 - b2) ** 2
+f5 = (b1 - B1) ** 2 + (b2 - B2) ** 2 + b3 ** 2 - l2 ** 2
+f6 = (c1 - C1) ** 2 + (c2 - C2) ** 2 + c3 ** 2 - l3 ** 2
+f7 = (a1 - B1) ** 2 + (a2 - B2) ** 2 - (a1 - C1) ** 2 - (a2 - C2) ** 2
+f8 = (b1 - A1) ** 2 + (b2 - A2) ** 2 - (b1 - C1) ** 2 - (b2 - C2) ** 2
+f9 = (c1 - A1) ** 2 + (c2 - A2) ** 2 - (c1 - B1) ** 2 - (c2 - B2) ** 2
 
 F = [f1, f2, f3, f4, f5, f6, f7, f8, f9]
 F_lambda = []
@@ -48,7 +42,7 @@ F_lambda = []
 for l in range(n):
     F_lambda.append(lambdify(x, F[l], 'numpy'))
 
-vec = [-7.5, 0, 2, 0, sqrt(168.75), 2, 7.5, 0, 2]
+vec = [0, 0, 5, 10, 17.32050807568877, 5, 20, 0, 5]
 
 print(sqrt(F_lambda[0](vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6], vec[7], vec[8])))
 print(sqrt(F_lambda[1](vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6], vec[7], vec[8])))
@@ -68,6 +62,8 @@ for i in range(n):
         F1[i].append(F[i].diff(x[l]))
         F1_lambda[i].append(lambdify(x, F1[i][l], 'numpy'))
 
+print(F1)
+
 F1_inv = [[] for i in range(n)]
 for i in range(n):
     for l in range(n):
@@ -76,7 +72,7 @@ for i in range(n):
 F1_inv = np.matrix(F1_inv, 'float')
 print(F1_inv)
 F1_inv = inv(F1_inv)
-print(F1_inv)
+# print(F1_inv)
 
 # F2 = []
 # F2_lambda = []
